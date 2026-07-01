325cbdb25c51c4eda165ddbc38e524fee9f5994a
Joe White <josephwhite.jfw@gmail.com>
2026-07-01 18:54:54 -0400

    allow play_slots() to take on all symbols and probs

--- slots.R
+++ slots.R
@@ -1,5 +1,11 @@
-symbols <- c("cherry", "lemon", "bar", "seven", 
-             "horseshoe", "crown", "bell", "clover")
+symbols <- c("cherry",
+             "lemon",
+             "bar",
+             "seven",
+             "horseshoe",
+             "crown",
+             "bell",
+             "clover")
 
 probs <- c(
   0.35,  # cherry
@@ -17,61 +23,61 @@ spin_slots <- function(symbols, probs){
 }
 
 play_slots <- function(){
-  # I want to set something up that is like, every $1 is 1 spin, if someone wagers $50 they get 50 spins,
   spin <- spin_slots(symbols, probs)
+  
   n_cherry <- sum(spin == "cherry")
   n_lemon <- sum(spin == "lemon")
   n_bar <- sum(spin == "bar")
   n_seven <- sum(spin == "seven")
+  n_horseshoe <- sum(spin == "horseshoe")
+  n_crown <- sum(spin == "crown")
+  n_bell <- sum(spin == "bell")
+  n_clover <- sum(spin == "clover")
   
-  if (n_seven == 4) {
-    payout = 100
-  } else if (n_lemon >= 3) {
-    payout = 15
-  } else if (n_bar >= 3) {
-    payout = 5
-  } else if (n_cherry >= 3) {
-    payout = 2
+  if (n_cherry == 3) {
+    payout <- 2
+  } else if (n_lemon == 3) {
+    payout <- 5
+  } else if (n_bar == 3) {
+    payout <- 10
+  } else if (n_seven == 3) {
+    payout <- 100 
+  } else if (n_horseshoe == 3) {
+    payout <- 50
+  } else if (n_crown == 3) {
+    payout <- 90
+  } else if (n_bell == 3) {
+    payout <- 80
+  } else if (n_clover == 3) {
+    payout <- 75
   } else {
-    payout = 0
+    payout <- -1
   }
-  
+
   return(list(
     spin = spin,
     winnings = payout
   ))
 }
 
-simulate_slots <- function(wager) {
-  payouts <- wager
+simulate_slots <- function(runs) {
+  results <- rep(NA, runs)
+  spins <- matrix(NA, nrow = runs, ncol = 3)
   
-  for (i in seq_len(wager)) {
+  for (i in seq_len(runs)) {
     result <- play_slots()
-    payouts[i] <- result$winnings
+    results[i] <- result$winnings
+    spins[i,] <- result$spin
   }
   
-  total_payout <- sum(payouts)
-  profits <- total_payout - wager
-  
+  total_payout <- sum(results)
+
   return(list(
-    spins = wager,
-    total_wagered = wager,
-    total_payout = total_payout,
-    profit = profits
+    runs = runs,
+    spins = spins,
+    total_payout = total_payout
   ))
 }
 
-simulate_slots(10000)
+simulate_slots(100)
 
-play_slots1 <- function(n) {
-  results <- matrix(NA, nrow = n, ncol = 3)
-  
-  for (i in seq_len(n)) {
-    result <- spin_slots(symbols, probs)
-    results[i, ] <- result
-  }
-  
-  return(results)
-}
-
-play_slots1(10)
