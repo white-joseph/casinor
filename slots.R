06a2b1bdef4d7b09493cc0bbddc67c5c9e3a72e4
Joe White <josephwhite.jfw@gmail.com>
2026-07-07 09:27:50 -0400

    early roxygen2 documentation

--- slots.R
+++ slots.R
@@ -23,40 +23,31 @@ spin_slots <- function(payouts, probs, reels = 3) {
   sample(names(payouts), reels, replace = TRUE, prob = probs)
 }
 
+#' Spin a slot machine and calculate profit given a wager
+#' 
+#' Takes a named vector of slot symbols, the payouts for each symbol, a wager, 
+#' and the number of reels. Returns a logical win, and profit if any. 
+#' 
+#' @param payouts Named vector of slot symbols and payouts. Name must be a character,
+#' and payout must be numeric
+#' @param probs A numeric vector of probabilities associated with each symbol.
+#' Must sum to 1 and be equal length of payouts
+#' @param wager Numeric of how much the user would like to wager. Profit is determined
+#' by wager and payouts.
+#' @param reels A numeric vector of length 1 defining how many slot  reels.
+#' Default is 3.
+#' 
+#' @return A list containg jackpot, which is a logical win/loss. Symbol, which if jackpot
+#' is true is the winning symbol, NA if else. Spin, the spin from the spin_slots function.
+#' And profit, calculated based on the payouts times the wager. 
+#' 
+#' @examples 
+#' play_slots(payouts = c(cherry = 1, seven = 5, bar = 15), probs = c(0.7, 0.2, 0.1))
+#' play_slots(payouts = c(cherry = 5, seven = 10), probs = c(0.75, 0.25), wager = 50)
+#' 
+#' @export
+
 play_slots <- function(payouts, probs, wager = 1, reels = 3) {
-  # Takes a named vector of payouts and returns if a jackpot was hit as well as the profit.
-  # 
-  # Parameters: 
-  # 
-  # payouts: a named vector where names correspond to the slot symbol and payout
-  #          is the corresponding payout for that symbol
-  #
-  # probs: the probability corresponding to the symbol. How likely that symbol
-  #        will be chosen
-  #
-  # wager: numeric; how much money the user wants to wager on a roll
-  #
-  # reels: the number of reels or windows on the slot machine. Default is 3.
-  #
-  # Returns: 
-  #
-  # list of if a jackpot occurred, what the spin was, if jackpot then the symbol, and profit.
-  # If there is no jackpot, then symbol defaults to be NA. Profit = wager times payout
-  #
-  # example: play_slots(payouts = c("cherry" = 1, "seven" = 5, "bar" = 15), probs = c(0.7, 0.2, 0.1))
-  #          may return: 
-  # $jackpot
-  # [1] FALSE
-  
-  # $spin
-  # [1] "seven"  "seven"  "cherry"
-  
-  # $symbol
-  # [1] NA
-  
-  # $profit
-  # [1] -1
-  
   spin <- spin_slots(payouts, probs, reels)
   jackpot <- (length(unique(spin)) ==  1)
   
@@ -76,29 +67,22 @@ play_slots <- function(payouts, probs, wager = 1, reels = 3) {
   ))
 }
 
+#' Simulate many spins of a slot machine
+#' 
+#' Takes a named vector of payouts and returns the results of n spins of a slot machine.
+#' 
+#' @param payouts Named vector of slot symbols and payouts. Name must be a character,
+#' and payout must be numeric
+#' @param probs A numeric vector of probabilities associated with each symbol.
+#' Must sum to 1 and be equal length of payouts
+#' @param runs A numberic of how many runs of the simulation.
+#' @param wager Numeric of how much the user would like to wager. Profit is determined
+#' by wager and payouts.
+#' @param reels A numeric vector of length 1 defining how many slot  reels.
+#' Default is 3.
+#' 
+
 simulate_slots <- function(payouts, probs, runs, wager = 1, reels = 3) {
-  # Takes a named vector of payouts, simulates n spins, and returns list of results
-  # 
-  # Parameters: 
-  # 
-  # payouts: a named vector where names correspond to the slot symbol and payout
-  #          is the corresponding payout for that symbol
-  #
-  # probs: the probability corresponding to the symbol. How likely that symbol
-  #        will be chosen
-  #
-  # runs: how many spins the simulation will run for
-  # 
-  # wager: numeric; how much money the user wants to wager on a roll. If no jackpot
-  #        occurs then the profit is a loss of the value of the wager.
-  #
-  # reels: the number of reels or windows on the slot machine. Default is 3.
-  # 
-  # Returns: 
-  #
-  # List: numeric of the number of runs, matrix of the spin results, numeric of
-  #       the number of jackpots, numeric of total payouts. Profit = wager times payout
-  
   spins <- matrix(NA, nrow = runs, ncol = reels)
   jackpots <- rep(NA, runs)
   profits <- rep(NA, runs)
