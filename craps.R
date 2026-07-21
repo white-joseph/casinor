0de0a28a46fb5c857e94db6a83dd630cb66469bc
Joe White <josephwhite.jfw@gmail.com>
2026-07-21 10:23:56 -0400

    Add profit functions

--- craps.R
+++ craps.R
@@ -2,103 +2,14 @@
 #'
 #' Rolls two fair die and returns an atomic vector of each roll
 #' 
-#' @return A numeric vector of length 2 where each element is an independent roll
-#' 
+#' @return A numeric vector of length 3 where the first 2 elements are the roll 
+#' of the respective die, and element 3 is their sum.
+#'  
 #' @examples 
 #' roll_die()
 #' roll_die()
 #' 
 #' @export
-
-roll_die <- function() {
-  die <- c(1:6)
-  roll <- sample(die, 2, replace = TRUE)
-  
-  return(roll)
-}
-
-#' Rolls the come out roll for a game of craps
-#'
-#' Rolls two fair die; Craps if their sum is 2,3, or 12. Establishes the point if
-#' their sum is not craps or 7 or 11.
-#' 
-#' @return A list where roll is a vector of length 2 where each element is an independent roll. 
-#' craps is a logical for if craps was hit, and the point is the value of the roll if the point was 
-#' established. Craps FALSE if sum of the rolls are 7 and 11. Point is NA if sum of the roll is
-#' 2,3,7,11 or 12.
-#' 
-#' @examples
-#' come_out_roll()
-#' come_out_roll()
-#' 
-#' @export
-
-come_out_roll <- function() {
-  roll <- roll_die()
-  
-  if (sum(roll) == 2 || sum(roll) == 3 || sum(roll) == 12) {
-    craps <- TRUE
-    point <- NA
-  } else if (sum(roll) == 7 || sum(roll) == 11) {
-    craps <- FALSE
-    point <- NA
-  } else {
-    craps <- FALSE
-    point <- sum(roll)
-  }
-  
-  return(list(
-    roll = roll,
-    craps = craps,
-    point = point
-  ))
-}
-
-resolve_point <- function(point) {
-  roll <- roll_die()
-  print(paste0("Roll: ", roll[1], ", ", roll[2]))
-  rolls <- matrix(roll, nrow = 1)
-  n_rolls <- 1
-  
-  while (sum(roll) != 7 && sum(roll) != point) {
-    roll <- roll_die()
-    print(paste0("Roll: ", roll[1], ", ", roll[2]))
-    rolls <- rbind(rolls, roll)
-    n_rolls <- n_rolls + 1
-    rownames(rolls) <- NULL
-  }
-  
-  return(list(
-    rolls = rolls,
-    n_rolls = n_rolls
-  ))
-}
-
-play_craps <- function(pass_line, dont_pass) {
-  come_out <- come_out_roll()
-  
-  if (come_out$craps) {
-    profit <- - pass_line + dont_pass
-  } else if (!come_out$craps && is.na(come_out$point)) {
-    profit <- pass_line - dont_pass
-  } else if (!is.na(come_out$point)) {
-    print(paste("Point established, point is:", come_out$point))
-    roll <- resolve_point(come_out$point)
-    
-    if(sum(roll$rolls[nrow(roll$rolls), ]) == 7) {
-      print("Seven out")
-      profit <- - pass_line + dont_pass
-    } else {
-      print("Hit the point")
-      profit <- pass_line - dont_pass
-    }
-  }
-  
-  return(list(
-    roll = come_out$roll,
-    profit = profit
-  ))
-}
 
 roll_die <- function() {
   die = c(1:6)
@@ -110,68 +21,151 @@ roll_die <- function() {
   return(roll)
 }
 
-come_out_roll <- function(pass_line_wager = 10, dont_pass_wager = 0) {
+come_out_roll <- function() {
+  
   roll <- roll_die()
   
-  if (roll["sum"] == 7 || roll["sum"] == 11) {
+  if (roll["sum"] %in% c(7,11)) {
+    outcome <- "natural"
     point <- NA
-    profit <- pass_line_wager - dont_pass_wager
-  } else if (roll["sum"] == 2 || roll["sum"] == 3) {
+  } else if (roll["sum"] %in% c(2,3)) {
+    outcome <- "craps"
     point <- NA
-    profit <- dont_pass_wager - pass_line_wager
   } else if (roll["sum"] == 12) {
+    outcome <- "push"
     point <- NA
-    profit <- -pass_line_wager
   } else {
+    outcome <- "point"
     point <- roll[["sum"]]
-    profit <- 0
   }
   
   return(list(
     roll = roll,
-    point = point,
-    profit = profit
+    outcome = outcome,
+    point = point
   ))
 }
 
+#' Rolls a die until their sum is 7 or the point value to resolve the point
+#' 
+#' Rolls two fair die until either their sum is 7 or the established point. Returns
+#' a matrix where each row is a specific roll. Returns a numeric vector of length 1
+#' which is the number of rolls it took to resolve the point.
+#' 
+#' @param point Whole numeric vector of length 1 who's value must be between 2 and 12 but not 7
+#' 
+#' @examples 
+#' resolve_point(4)
+#' resolve_point(6)
+#' 
+#' @export
+
 resolve_point <- function(point) {
+  
+  if (!is.numeric(point) || length(point) != 1) {
+    stop("point must be numeric and length 1.")
+  }
+  
+  if (point < 2 || point > 12 || point == 7) {
+    stop("point must be between 2 and 12 but not 7.")
+  }
+  
   roll <- roll_die()
   n_rolls <- 1
   rolls <- matrix(roll, nrow = 1)
+  colnames(rolls) <- c("die1", "die2", "sum")
   
   while (roll["sum"] != 7 && roll["sum"] != point) {
     roll <- roll_die()
     n_rolls <- n_rolls + 1
     rolls <- rbind(rolls, roll)
-    colnames(rolls) <-  c("die1", "die2", "sum")
     rownames(rolls) <- NULL
+  }
+  
+  if (rolls[nrow(rolls), "sum"] == 7) {
+    outcome <- "seven_out"
+  } else {
+    outcome <- "point_made"
   }
   
   return(list(
     rolls = rolls,
+    outcome = outcome,
     n_rolls = n_rolls
   ))
 }
 
-play_craps <- function(pass_line_wager, dont_pass_wager) {
-  come_out <- come_out_roll(pass_line_wager, dont_pass_wager)
-  roll <- come_out$roll
-  profit <- come_out$profit
+pass_line_profit <- function(outcome, pass_line_wager) {
   
-  if (!is.na(come_out$point)) {
-    resolve_point <- resolve_point(come_out$point)
-    rolls <- resolve_point$rolls
-    
-    if (rolls[nrow(rolls), "sum"] == 7) {
-      profit = dont_pass_wager - pass_line_wager
-    } else {
-      profit = pass_line_wager - dont_pass_wager
-    }
+  if (outcome %in% c("natural", "point_made")) {
+    profit <- pass_line_wager
+  } else if (outcome %in% c("craps", "push", "seven_out")) {
+    profit <- -pass_line_wager
   }
   
+  return(profit)
+}
+
+dont_pass_profit <- function(outcome, dont_pass_wager) {
+  
+  if (outcome %in% c("craps", "seven_out")) {
+    profit <- dont_pass_wager
+  } else if (outcome %in% c("natural", "point_made")) {
+    profit <- -dont_pass_wager
+  } else if (outcome == "push") {
+    profit <- 0
+  }
+  
+  return(profit)
+}
+
+odds_profit <- function(point, outcome, odds_wager) {
+  
+  if (outcome == "point_made"  && point %in% c(4,10)) {
+    profit <- 2*odds_wager
+  } else if (outcome == "point_made" && point %in% c(5,9)) {
+    profit <- (3/2)*odds_wager
+  } else if (outcome == "point_made" && point %in% c(6,8)) {
+    profit <- (6/5)*odds_wager
+  } else {
+    profit <- -odds_wager
+  }
+  
+  return(profit)
+}
+
+play_craps <- function(pass_line_wager = 10, dont_pass_wager = 0, odds_wager = 0) {
+  come_out <- come_out_roll()
+  
+  if(come_out$outcome != "point") {
+    
+    pass_line <- pass_line_profit(come_out$outcome, pass_line_wager)
+    dont_pass <- dont_pass_profit(come_out$outcome, dont_pass_wager)
+    
+    profit <- sum(pass_line, dont_pass)
+    
+    return(list(
+      roll = come_out$roll,
+      outcome = come_out$outcome,
+      profit = profit
+    ))
+  }
+  
+  resolution <- resolve_point(come_out$point)
+  rolls <- resolution$rolls
+  
+  pass_line <- pass_line_profit(resolution$outcome, pass_line_wager)
+  dont_pass <- dont_pass_profit(resolution$outcome, dont_pass_wager)
+  odds <- odds_profit(come_out$point, resolution$outcome, odds_wager)
+  
+  profit <- sum(pass_line, dont_pass, odds)
+  
   return(list(
-    roll = roll,
+    roll = come_out$roll,
+    point = come_out$point,
     rolls = rolls,
+    outcome = resolution$outcome,
     profit = profit
   ))
 }
+
